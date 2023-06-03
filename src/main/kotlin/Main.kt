da5f9fb5d05da021eae18098241f872a482b5865
Dmytro Kovtunov <dmytro.kovtunov@rio.cloud>
2023-06-03 23:18:10 +0200

    misc

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,7 @@
 fun main(args: Array<String>) {
-    println(
-        PhoneBook(
-            listOf(
-                Contact("User1", "1788972359876"),
-                Contact("User2", "1771242151435")
-            )
-        ).search("17")
-    )
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
 }
