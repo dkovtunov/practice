4efa9985769d574c4f99d8b934f564e77a725821
Dmytro Kovtunov <dmytro.kovtunov@rio.cloud>
2023-06-06 22:40:32 +0200

    buggy first missing positive

--- typescript/src/prevPositive.ts
+++ typescript/src/prevPositive.ts
@@ -31,30 +31,32 @@ each element of array A is an integer within the range [−1,000,000..1,000,000]
 
 const MAX_INT_BITS = 53
 
-export function solution(A: number[]): number {
+export function solution(nums: number[]): number {
   const masks: Map<number, number> = new Map()
 
-  A.forEach(v => {
+  nums.forEach(v => {
+    if (v < 1) return
+
     const cell = Math.floor(v / MAX_INT_BITS)
-    const bitPosition = v % MAX_INT_BITS
+    const bitPosition = v % MAX_INT_BITS - 1
     const prev = masks.get(cell)
 
     masks.set(cell, 1 << bitPosition | (prev == undefined ? 0 : prev))
   })
 
-  const keys = Object.keys(masks).map(Number)
-  keys.sort()
+  const keys = Array.from(masks.keys())
 
   if (keys[0] != 0) return 1
 
   const key = keys.find(k => masks.get(k) != Number.MAX_SAFE_INTEGER)
 
-  if (key != null) {
-    let result = -1
-    const value = masks.get(key)!!
-    do {
+  if (key != undefined) {
+    let result = 1
+    let value = masks.get(key)!!
+    while ((value & 1) !== 0) {
       result++
-    } while ((value & 1) !== 0)
+      value >>= 1
+    }
 
     return result + (key * MAX_INT_BITS)
   }
